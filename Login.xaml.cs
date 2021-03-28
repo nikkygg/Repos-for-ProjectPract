417891f2dfeecb52c47c7a34b7be23c5e5bb8b9d
Nikita <Nikita12345Pro@yandex.ru>
2021-03-28 04:54:14 +0300

    Files

---
+++ Login.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace ProjectPharmacy
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        public Window1()
+        {
+            InitializeComponent();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString.ToString();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+        private bool VerifyUser(string username, string password)
+        {
+            con.Open();
+            com.Connection = con;
+            com.CommandText = "select Status from Users where Username='" + username + "' and password='" + password + "'";
+            dr = com.ExecuteReader();
+            if (dr.Read())
+            {
+                if (Convert.ToBoolean(dr["Status"]) == true)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+            if (VerifyUser(txtUsername.Text, txtPassword.Password))
+            {
+                MessageBox.Show("Вы успешно вошли в систему!", "Успешно", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                Window1 taskWindow = new Window1();
+                taskWindow.Close();
+
+
+            }
+            else
+            {
+                MessageBox.Show("Некорректный ввод данных! Проверьте правильность данных!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    } 
+}
